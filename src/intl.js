a91587398f76265cef6059b270232582045e991a
xiaoyunhan <xiaoyunhan@momenta.ai>
2020-10-26 15:36:07 +0800

    improve intl translate

--- src/intl.js
+++ src/intl.js
@@ -1,94 +1,112 @@
-import React from 'react'
-import fmt from 'string-format'
+import React from "react";
+import fmt from "string-format";
 
-const KEY_LS_LOCALE = 'locale'
+const KEY_LS_LOCALE = "locale";
 
-let current = 'zh'
-let fallback = 'en'
-let handlers = []
-let global = {}
+let current = "zh-CN";
+let fallback = "en-US";
+let handlers = [];
+let global = {};
 
-const locale_from_ls = localStorage.getItem(KEY_LS_LOCALE)
+const locale_from_ls = localStorage.getItem(KEY_LS_LOCALE);
 
-if (locale_from_ls != null && locale_from_ls.trim() != '') {
-  current = locale_from_ls
+if (locale_from_ls != null && locale_from_ls.trim() != "") {
+  current = locale_from_ls;
 }
 
-const LocaleContext = React.createContext(current)
+const LocaleContext = React.createContext(current);
 
-const createTranslate = local => {
-  const languages = { ...global, ...local }
+const createTranslate = (local) => {
+  const languages = { ...global, ...local };
   return (key, ...params) => {
-    const item = languages[key]
-    if (item === undefined) {
-      throw new Error(`intl:Can't find the key [${key}] in locales`)
-    }
-    let text = item[current]
-    if (text === undefined) {
-      text = item[fallback]
-    }
-    if (text === undefined || typeof text !== 'string') {
-      throw new Error(`intl:No languages(${current},${fallback}) for the key[${key}]`)
-    }
-    return fmt(text, ...params)
-  }
-}
+    const item = languages[key];
 
-const intl = target => props => <LocaleContext.Consumer>{value => React.createElement(target, { ...props, locale: value })}</LocaleContext.Consumer>
+    let text = item[current];
+    if (!(key in languages)) {
+      new Error(
+        `intl:No languages(${current},${fallback}) for the key[${key}]`
+      );
+      text = "NO KEY";
+    } else {
+      if (text === undefined) {
+        text = "undefined";
+      } else if (text === null) {
+        text = "null";
+      } else if (typeof text !== "string") {
+        new Error(`Init: [${key}]: value is not a string or number in locales`);
+        text = "-";
+      }
+    }
+    // if (text === undefined || typeof text !== 'string') {
+    //   throw new Error(`intl:No languages(${current},${fallback}) for the key[${key}]`)
+    // }
+    return fmt(text, ...params);
+  };
+};
+
+const intl = (target) => (props) => (
+  <LocaleContext.Consumer>
+    {(value) => React.createElement(target, { ...props, locale: value })}
+  </LocaleContext.Consumer>
+);
 
 intl.locale = (...args) => {
-  if (args.length === 0) return current
+  if (args.length === 0) return current;
   else {
-    const [currentLocale, fallbackLocale] = args
-    if (current === currentLocale && fallback === fallbackLocale) return
+    const [currentLocale, fallbackLocale] = args;
+    if (current === currentLocale && fallback === fallbackLocale) return;
     setTimeout(() => {
-      current = currentLocale
-      fallback = fallbackLocale
-      localStorage.setItem(KEY_LS_LOCALE, current)
-      handlers.forEach(handler => handler(current))
-    })
+      current = currentLocale;
+      fallback = fallbackLocale;
+      localStorage.setItem(KEY_LS_LOCALE, current);
+      handlers.forEach((handler) => handler(current));
+    });
   }
-}
+};
 
-intl.global = languages => {
-  global = languages || {}
-}
+intl.global = (languages) => {
+  global = languages || {};
+};
 
-intl.onChanged = handler => {
-  if (handler && typeof handler === 'function') {
-    handlers.push(handler)
+intl.onChanged = (handler) => {
+  if (handler && typeof handler === "function") {
+    handlers.push(handler);
   } else {
-    throw Error('intl:The parameter of intl.onChanged must be a function')
+    throw Error("intl:The parameter of intl.onChanged must be a function");
   }
   return () => {
-    handlers = handlers.filter(f => f !== handler)
-  }
-}
+    handlers = handlers.filter((f) => f !== handler);
+  };
+};
 
-intl.load = languages => createTranslate(languages)
+intl.load = (languages) => createTranslate(languages);
 
 intl.LocaleContextProvider = class extends React.PureComponent {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      locale: current
-    }
-    this.removeHandler = intl.onChanged(this.handleChange.bind(this))
+      locale: current,
+    };
+    this.removeHandler = intl.onChanged(this.handleChange.bind(this));
   }
 
   componentWillUnmount() {
-    this.removeHandler()
+    this.removeHandler();
   }
 
   handleChange(locale) {
-    if (this.state.locale === locale) return
-    this.setState({ locale })
+    if (this.state.locale === locale) return;
+    this.setState({ locale });
   }
 
   render() {
-    if (this.state.locale == null) return null
-    return <LocaleContext.Provider value={this.state.locale}>{this.props.children}</LocaleContext.Provider>
+    if (this.state.locale == null) return null;
+    return (
+      <LocaleContext.Provider value={this.state.locale}>
+        {this.props.children}
+      </LocaleContext.Provider>
+    );
   }
-}
+};
 
-export default intl
+export default intl;
