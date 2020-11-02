d7a8bef5e4f38896da88f8cbd9807c4d9a4decc6
xiaoyunhan <xiaoyunhan@momenta.ai>
2020-11-02 20:01:54 +0800

    improve dynamic .d.ts type

--- dynamic.d.ts
+++ dynamic.d.ts
@@ -1,2 +1,9 @@
-declare const dynamic: (resolve: (value?: PromiseLike<any>) => void) => void;
-export default dynamic;
+// declare const dynamic: (resolve: (value?: PromiseLike<any>) => void) => void;
+// export default dynamic;
+
+declare class dynamic {
+    constructor(resolve: (value?: PromiseLike<any>) => void)
+    static setDefaultLoadingComponent: (c?: React.ElementType) => void
+}
+
+export default dynamic
